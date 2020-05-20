779097eb36c492da70173ce872fe0c4598e5ac4e
Arthur Siqueira <arthur.siq5@gmail.com>
2020-05-20 12:47:52 -0300

    mostrando no console dados puxados da API

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,5 +1,9 @@
 $("#botao-frase").click(fraseAleatoria);
 
 function fraseAleatoria() {
+    $.get("http://localhost:3000/frases", trocaFraseAleatoria);
+}
 
+function trocaFraseAleatoria(data) {
+    console.log(data);
 }
