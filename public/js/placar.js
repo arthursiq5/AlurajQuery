31114ebf028fde164df7846653a32c8570c1ee7e
Arthur Siqueira <arthur.siq5@gmail.com>
2020-05-20 12:32:40 -0300

    suavizando ainda mais a remoção

--- public/js/placar.js
+++ public/js/placar.js
@@ -12,8 +12,10 @@ function removeLinha(event) {
     event.preventDefault();
     let linha = $(this).parent().parent();
 
-    linha.fadeOut();
-    linha.remove();
+    linha.fadeOut(1000);
+    setTimeout(function () {
+        linha.remove();
+    }, 1000);
 }
 
 function novaLinha(usuario, palavras) {
