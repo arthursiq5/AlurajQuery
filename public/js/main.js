e831e8bff2b18ff4e06bfcd871879ecbf5449101
Arthur Siqueira <arthur.siq5@gmail.com>
2020-05-19 22:53:14 -0300

    importadas dependências e modularizado javascript

--- public/js/main.js
+++ public/js/main.js
@@ -67,41 +67,6 @@ function reiniciaJogo() {
 }
 $("#botao-reiniciar").click(reiniciaJogo);
 
-function inserePlacar() {
-    var corpoTabela = $(".placar").find("tbody");
-    var usuario = "Seu-nome";
-    var numPalavras = $("#contador-palavras").text();
-    let linha = novaLinha(usuario, numPalavras);
-    linha.find(".botao-remover").click(removeLinha);
-
-    corpoTabela.prepend(linha);
-}
-
-function removeLinha(event) {
-    event.preventDefault();
-    $(this).parent().parent().remove();
-}
-
-function novaLinha(usuario, palavras) {
-    var linha = $("<tr>");
-    var colunaUsuario = $("<td>").text(usuario);
-    var colunaPalavras = $("<td>").text(palavras);
-    var colunaRemover = $("<td>");
-
-    var link = $("<a>").attr("href", "#").addClass("botao-remover");
-    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
-
-    link.append(icone);
-
-    colunaRemover.append(link);
-
-    linha.append(colunaUsuario);
-    linha.append(colunaPalavras);
-    linha.append(colunaRemover);
-
-    return linha;
-}
-
 $(document).ready(function () {
     atualizaTamanhoFrase();
     inicializaContadores();
