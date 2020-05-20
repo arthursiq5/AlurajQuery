a2b3e5502ef7ef86b46b82702359cffdd974960c
Arthur Siqueira <arthur.siq5@gmail.com>
2020-05-19 21:55:21 -0300

    encapsulando código em função

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,21 @@
 var tempoInicial = $("#tempo-digitacao").text();
 var campo = $(".campo-digitacao");
-var frase = $(".frase").text();
 
-campo.on("input", function () {
-    var digitado = campo.val();
-    var comparavel = frase.substr(0, digitado.length);
+function inicializaMarcadores() {
+    var frase = $(".frase").text();
+    campo.on("input", function () {
+        var digitado = campo.val();
+        var comparavel = frase.substr(0, digitado.length);
 
-    if (digitado == comparavel) {
-        campo.addClass("borda-verde");
-    } else {
-        campo.addClass("borda-vermelha");
-    }
-});
+        if (digitado == comparavel) {
+            campo.addClass("borda-verde");
+            campo.removeClass("borda-vermelha");
+        } else {
+            campo.addClass("borda-vermelha");
+            campo.removeClass("borda-verde");
+        }
+    });
+}
 
 function atualizaTamanhoFrase(){
     let frase = $(".frase").text();
@@ -66,5 +70,6 @@ $(document).ready(function () {
     atualizaTamanhoFrase();
     inicializaContadores();
     inicializaCronometro();
+    inicializaMarcadores();
     $("#botao-reiniciar").click(reiniciaJogo);
 });
